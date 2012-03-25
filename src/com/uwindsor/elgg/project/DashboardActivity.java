1e43c3d2544c05862aacf8ce53570871fb22514a
Matt Colthurst <Matt@Matt-Laptop.(none)>
2012-03-25 16:08:38 -0400

    Minor update forgot a few things in the last one

--- src/com/uwindsor/elgg/project/DashboardActivity.java
+++ src/com/uwindsor/elgg/project/DashboardActivity.java
@@ -8,6 +8,6 @@ public class DashboardActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.dashboard);
+		setContentView(R.layout.dashboard);
 	}
 }
