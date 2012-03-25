7d350f362024adf5c0d36580f732359a1b420b39
Matt Colthurst <Matt@Matt-Laptop.(none)>
2012-03-25 16:02:56 -0400

    Changed Scope now making android app to connect to Elgg site. Using two open source plugins for api calls they are included in the Plugins folder and links to them will be in the documentation. Also included is the framework of the app so far it is just the mainActivity and some http utils

---
+++ src/com/uwindsor/elgg/project/MainActivity.java
@@ -0,0 +1,47 @@
+package com.uwindsor.elgg.project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	EditText uname;
+	EditText pwd;
+	Button login;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        uname = (EditText) findViewById(R.id.uName);
+        pwd = (EditText) findViewById(R.id.passWD);
+        login = (Button) findViewById(R.id.loginBtn);
+        
+        login.setOnClickListener(btnListener);
+        
+    }
+    
+    private OnClickListener btnListener = new OnClickListener() {
+        public void onClick(View v) {
+        	if(uname.getText().toString().isEmpty())
+        		Toast.makeText(getApplicationContext(), "Invalid User Name", Toast.LENGTH_LONG);
+        	else if(pwd.getText().toString().isEmpty())
+        		Toast.makeText(getApplicationContext(), "Invalid Password", Toast.LENGTH_LONG);
+        	else{
+        			Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
+        			i.putExtra("uname", uname.getText().toString());
+        			i.putExtra("pwd", pwd.getText().toString());
+        			startActivity(i);
+        		}
+        }
+    };
+}
